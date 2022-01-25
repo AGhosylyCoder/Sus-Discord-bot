9815d882f76ed2dc07fd019476fe8e4fb946b035
unknown <olghostlylo@gmail.com>
2022-01-25 16:27:51 +0800

    base + welcome

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const Discord = require("discord.js")
+const generateImage = require("./generateImage")
 require("dotenv").config()
+
+const genereateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -17,6 +21,21 @@ client.on("messageCreate", (message) => {
     {
         message.reply("Hello, World!")
     }
+    if(message.content == "It's ALIVE!" || message.content == "its alive!")
+    {
+        message.reply("Yes, I am")
+    }
+})
+
+const welcomeChannelId = "935413324835848202"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+    content: `<@${member.id}> Welcome to the server!`,
+    files: [img]
+})
+    
 })
 
 client.login(process.env.TOKEN)
